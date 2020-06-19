92290c15c27d024589ee0eb488ff6165802bdd44
Nadja Popovic <45973850+nadjas515@users.noreply.github.com>
2020-06-19 13:49:53 +0200

    Add files via upload

---
+++ ArduCAM_ESP8266_n.ino
@@ -0,0 +1,241 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include "memorysaver.h"
+
+#if !(defined OV5642_MINI_5MP || defined OV5642_MINI_5MP_BIT_ROTATION_FIXED || defined OV2640_MINI_2MP || defined OV3640_MINI_3MP)
+  #error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
+#endif
+
+const int CS = D0;
+
+bool is_header = false;
+ArduCAM myCAM( OV2640, CS );
+//Default is arducam_esp8266.If you want,you can change the AP_aaid  to your favorite name
+const char *AP_ssid = "arducam_esp8266";
+//Default is no password.If you want to set password,put your password here
+const char *AP_password = "";
+
+ESP8266WebServer server(80);
+
+uint8_t read_fifo_burst(ArduCAM myCAM);
+
+
+void start_capture() {
+  myCAM.clear_fifo_flag();
+  myCAM.start_capture();
+}
+
+void camCapture(ArduCAM myCAM) {
+  WiFiClient client = server.client();
+
+  uint8_t temp = 0, temp_last = 0;
+  size_t len = myCAM.read_fifo_length();
+  if (len >= 0x07ffff) {
+    Serial.println("Over size.");
+    return;
+  } else if (len == 0 ) {
+    Serial.println("Size is 0.");
+    return;
+  }
+
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst();
+  if (!client.connected()) return;
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: image/jpeg\r\n";
+  response += "Content-Length: " + String(len) + "\r\n\r\n";
+  server.sendContent(response);
+
+  temp =  SPI.transfer(0x00);
+  len --;
+  while ( len-- )
+  {
+    temp_last = temp;
+    temp =  SPI.transfer(0x00);
+    if (is_header == true)
+    {
+      client.write(temp);
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      client.write(temp_last);
+      client.write(temp);
+    }
+    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+      break;
+    delayMicroseconds(15);
+  }
+
+  myCAM.CS_HIGH();
+  is_header=false;
+}
+
+void serverCapture() {
+  start_capture();
+  Serial.println("CAM Capturing");
+
+  int total_time = 0;
+
+  total_time = millis();
+  while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+  total_time = millis() - total_time;
+  Serial.print("capture total_time used (in miliseconds):");
+  Serial.println(total_time, DEC);
+
+  total_time = 0;
+
+  Serial.println("CAM Capture Done!");
+  total_time = millis();
+  camCapture(myCAM);
+  total_time = millis() - total_time;
+  Serial.print("send total_time used (in miliseconds):");
+  Serial.println(total_time, DEC);
+  Serial.println("CAM send Done!");
+}
+
+void serverStream() {
+  WiFiClient client = server.client();
+
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+
+  while (1) {
+    start_capture();
+
+    while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+
+    size_t len = myCAM.read_fifo_length();
+    if (len >= 0x07ffff) {
+      Serial.println("Over size.");
+      continue;
+    } else if (len == 0 ) {
+      Serial.println("Size is 0.");
+      continue;
+    }
+
+    myCAM.CS_LOW();
+    myCAM.set_fifo_burst();
+
+    if (!client.connected()) break;
+    response = "--frame\r\n";
+    response += "Content-Type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+
+    static const size_t bufferSize = 4096;
+    static uint8_t buffer[bufferSize] = {0xFF};
+
+    while (len) {
+      size_t will_copy = (len < bufferSize) ? len : bufferSize;
+      SPI.transferBytes(&buffer[0], &buffer[0], will_copy);
+      if (!client.connected()) break;
+      client.write(&buffer[0], will_copy);
+      len -= will_copy;
+    }
+    myCAM.CS_HIGH();
+
+    if (!client.connected()) break;
+  }
+}
+
+void handleNotFound() {
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text/plain", message);
+
+  if (server.hasArg("ql")) {
+    int ql = server.arg("ql").toInt();
+    myCAM.OV2640_set_JPEG_size(ql); delay(1000);
+    Serial.println("QL change to: " + server.arg("ql"));
+  }
+}
+
+void setup() {
+
+  
+  // put your setup code here, to run once:
+  uint8_t vid, pid;
+  uint8_t temp;
+  #if defined(__SAM3X8E__)
+    Wire1.begin();
+    Serial.begin(115200);
+  #else
+    Wire.begin();
+    Serial.begin(115200);
+  #endif
+
+  // setup AP
+
+   WiFi.mode(WIFI_AP);
+   WiFi.softAP(AP_ssid, AP_password);
+   Serial.println("");
+   Serial.println(WiFi.softAPIP());
+
+
+  pinMode(CS, OUTPUT);
+  digitalWrite(CS, 1);
+
+  SPI.begin();
+
+  // reset the CPLD
+  myCAM.write_reg(0x07,0x80);
+  delay(100);
+  myCAM.write_reg(0x07, 0x00);
+  delay(100);
+ while (1) {
+    //Check if the ArduCAM SPI bus is OK
+    myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+    temp = myCAM.read_reg(ARDUCHIP_TEST1);
+    if (temp != 0x55) {
+      Serial.println(F("ACK CMD SPI interface Error! END"));
+      delay(1000); continue;
+    } else {
+      Serial.println(F("ACK CMD SPI interface OK. END")); break;
+    }
+  }
+
+  // vidi postoji li kamera odgovarajuceg tipa
+  while (1) {
+    //Check if the camera module type is OV2640
+    myCAM.wrSensorReg8_8(0xff, 0x01);
+    myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+    myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+    if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 ))) {
+      Serial.println(F("ACK CMD Can't find OV2640 module! END"));
+      delay(1000); continue;
+    }
+    else {
+      Serial.println(F("ACK CMD OV2640 detected. END")); break;
+    }
+  }
+
+  myCAM.set_format(JPEG);
+  myCAM.InitCAM();
+  myCAM.OV2640_set_JPEG_size(OV2640_320x240);
+  delay(1000);
+  myCAM.clear_fifo_flag();
+
+  
+  server.on("/capture", HTTP_GET, serverCapture);
+  server.on("/stream", HTTP_GET, serverStream);
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  server.handleClient();
+}
---
+++ LED_n.ino
@@ -0,0 +1,100 @@
+//4.14.2019 1:07PM Prijedor
+//Nađa Popović
+
+int LP1=3;
+int LP2=4;
+int LP3=5;
+int LC1=6;
+int LC2=7;
+int LC3=8;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LP1,OUTPUT);
+pinMode(LP2,OUTPUT);
+pinMode(LP3,OUTPUT);
+pinMode(LC1,OUTPUT);
+pinMode(LC2,OUTPUT);
+pinMode(LC3,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LP1,HIGH);
+delay(1000);
+digitalWrite(LP1,LOW);
+digitalWrite(LP2,HIGH);
+delay(1000);
+digitalWrite(LP2,LOW);
+digitalWrite(LP3,HIGH);
+delay(1000);
+digitalWrite(LP3,LOW);
+digitalWrite(LC1,HIGH);
+delay(1000);
+digitalWrite(LC1,LOW);
+digitalWrite(LC2,HIGH);
+delay(1000);
+digitalWrite(LC2,LOW);
+digitalWrite(LC3,HIGH);
+delay(1000);
+digitalWrite(LC3,LOW);
+
+digitalWrite(LP1,HIGH);
+delay(3000);
+digitalWrite(LP2,HIGH);
+delay(2000);
+digitalWrite(LP3,HIGH);
+delay(1000);
+digitalWrite(LP1,LOW);
+digitalWrite(LP2,LOW);
+digitalWrite(LP3,LOW);
+delay(1000);
+digitalWrite(LP1,HIGH);
+digitalWrite(LP2,HIGH);
+digitalWrite(LP3,HIGH);
+delay(1000);
+digitalWrite(LP1,LOW);
+digitalWrite(LP2,LOW);
+digitalWrite(LP3,LOW);
+delay(1000);
+digitalWrite(LP1,HIGH);
+digitalWrite(LP2,HIGH);
+digitalWrite(LP3,HIGH);
+delay(1000);
+digitalWrite(LP1,LOW);
+delay(3000);
+digitalWrite(LP2,LOW);
+delay(2000);
+digitalWrite(LP3,LOW);
+delay(1000);
+
+digitalWrite(LC1,HIGH);
+delay(3000);
+digitalWrite(LC2,HIGH);
+delay(2000);
+digitalWrite(LC3,HIGH);
+delay(1000);
+digitalWrite(LC1,LOW);
+digitalWrite(LC2,LOW);
+digitalWrite(LC3,LOW);
+delay(1000);
+digitalWrite(LC1,HIGH);
+digitalWrite(LC2,HIGH);
+digitalWrite(LC3,HIGH);
+delay(1000);
+digitalWrite(LC1,LOW);
+digitalWrite(LC2,LOW);
+digitalWrite(LC3,LOW);
+delay(1000);
+digitalWrite(LC1,HIGH);
+digitalWrite(LC2,HIGH);
+digitalWrite(LC3,HIGH);
+delay(1000);
+digitalWrite(LC1,LOW);
+delay(3000);
+digitalWrite(LC2,LOW);
+delay(2000);
+digitalWrite(LC3,LOW);
+delay(1000);
+}
